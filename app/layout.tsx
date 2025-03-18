edc8e966b08324741f24b0126ca9e8a506851b03
태양 <zxcv2987@naver.com>
2025-03-18 20:44:13 +0900

    chore: font 추가 및 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,23 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "동글",
   description: "우리의 동아리, 우리의 동글",
 };
-
+const pretendard = localFont({
+  src: "../public/fonts/PretendardVariable.ttf",
+  display: "swap",
+  weight: "100 900",
+});
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pretendard.className}>
       <body>{children}</body>
     </html>
   );
