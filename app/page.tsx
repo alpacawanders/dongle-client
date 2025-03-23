967be8e8196ed406467595fde7fe15a6f51bee70
태양 <zxcv2987@naver.com>
2025-03-23 16:10:00 +0900

    feature: Main Page UI 작성 - mock 데이터와 함께 UI 작성완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,38 @@
+import Banner from "@/components/domain/main/banner/Banner";
+import ClubList from "@/components/domain/main/clubList/ClubList";
+import { ClubType } from "@/types/club";
+
 export default function Page() {
-  return <></>;
+  const mocks: ClubType[] = [
+    {
+      id: 1,
+      name: "가나다라",
+      category: "가나다라",
+      isRecruiting: true,
+    },
+    {
+      id: 2,
+      name: "나다라마",
+      category: "나다라마",
+      isRecruiting: false,
+    },
+    {
+      id: 3,
+      name: "다라마바",
+      category: "다라마바",
+      isRecruiting: true,
+    },
+    {
+      id: 4,
+      name: "라마바사",
+      category: "라마바사",
+      isRecruiting: false,
+    },
+  ];
+  return (
+    <div className="flex w-full flex-col gap-12 py-12 pb-48">
+      <Banner />
+      <ClubList clubs={mocks} />
+    </div>
+  );
 }
---
+++ components/domain/main/banner/Banner.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import ChevronRightIcon from "@/assets/images/chevronRight.svg";
+import ChevronLeftIcon from "@/assets/images/chevronLeft.svg";
+import mock1 from "@/assets/images/mocks/mock1.jpg";
+import mock2 from "@/assets/images/mocks/mock2.jpg";
+import mock3 from "@/assets/images/mocks/mock3.jpg";
+import clsx from "clsx";
+
+export default function Banner() {
+  const images = [mock1, mock2, mock3];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  return (
+    <article className="relative flex h-52 w-full overflow-hidden rounded-3xl bg-zinc-100">
+      <div
+        className="flex h-full w-full transition-transform duration-500 ease-in-out"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {images.map((image, index) => (
+          <div key={index} className="relative h-full w-full flex-shrink-0">
+            <Image
+              priority={index === 0}
+              src={image}
+              alt="Banner Image"
+              fill
+              className="object-cover"
+            />
+          </div>
+        ))}
+      </div>
+
+      <div className="absolute inset-0 z-10 flex items-center justify-between px-4">
+        <button
+          onClick={() =>
+            setCurrentIndex((prev) =>
+              prev === 0 ? images.length - 1 : prev - 1,
+            )
+          }
+        >
+          <ChevronLeftIcon />
+        </button>
+        <button
+          onClick={() =>
+            setCurrentIndex((prev) =>
+              prev === images.length - 1 ? 0 : prev + 1,
+            )
+          }
+        >
+          <ChevronRightIcon />
+        </button>
+      </div>
+
+      <div className="absolute bottom-3 left-1/2 flex -translate-x-1/2 space-x-2">
+        {images.map((_, index) => (
+          <span
+            onClick={() => setCurrentIndex(index)}
+            key={index}
+            className={clsx(
+              "h-2 w-2 cursor-pointer rounded-full transition-all",
+              currentIndex === index ? "w-3 bg-white" : "bg-zinc-400",
+            )}
+          />
+        ))}
+      </div>
+    </article>
+  );
+}
