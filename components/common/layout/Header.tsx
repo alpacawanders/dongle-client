c503250c890ed3d9bac2e72c98c3b99352ea94c2
태양 <zxcv2987@naver.com>
2025-03-26 23:57:38 +0900

    fix: Header href 수정

--- components/common/layout/Header.tsx
+++ components/common/layout/Header.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 import NavItem from "@/components/common/layout/NavItem";
 export default function Header() {
   const items = [
-    { name: "공지사항", href: "/" },
-    { name: "활동보고서", href: "/" },
+    { name: "공지사항", href: "/notice" },
+    { name: "활동보고서", href: "/reports" },
   ];
   return (
-    <header className="bg-white border-zinc-100 fixed left-0 top-0 z-50 h-20 w-full border-b md:px-0">
+    <header className="fixed left-0 top-0 z-50 h-20 w-full border-b border-zinc-100 bg-white md:px-0">
       <div className="m-[0_auto] flex h-full max-w-[960px] flex-row items-center justify-between">
         <Link href="/" className="h-7 w-20">
           <Logo />
         </Link>
-        <nav className="text-zinc-400 flex flex-row gap-3">
+        <nav className="flex flex-row gap-3 text-zinc-400">
           {items.map((item, i) => (
             <NavItem key={i} href={item.href}>
               {item.name}
